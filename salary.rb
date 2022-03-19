9656d538a1490dbcb155fd6119c6423a449af844
mbdev8993 <bablu.bhumca14@gmail.com>
2022-03-19 03:23:18 -0700

    Added data to salary.rb file

--- salary.rb
+++ salary.rb
@@ -4,5 +4,6 @@
 # this will also calculate highest and lowest salary getting emloyee.
 
 
-emp_id         salary         email         manager_id
+emp_id         salary         email                      manager_id
+1               23000         bablu.bhumca2014@mail.com  3
 
